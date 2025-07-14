59104c7bf8d81665c855ae610e7b5045333cac25
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-07-14 13:30:08 +0300

    set new res headers

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import request from 'request';
+import http from "http";
+import express from "express";
 
 const app = express();
-app.use(cors());
 
-const MJPEG_SOURCE = 'http://212.112.136.4:83/mjpg/video.mjpg?camera=1';
+const MJPEG_URL = "http://212.112.136.4:83/mjpg/video.mjpg?camera=1";
 
-app.get('/', (req, res) => {
-	req.pipe(request(MJPEG_SOURCE)).pipe(res);
+app.get("/cam", (req, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Content-Type", "multipart/x-mixed-replace; boundary=--myboundary");
+	res.setHeader("Cache-Control", "no-cache");
+	res.setHeader("Connection", "close");
+
+	const mjpegReq = http.get(MJPEG_URL, (mjpegRes) => {
+		mjpegRes.pipe(res);
+	});
+
+	mjpegReq.on("error", (err) => {
+		console.error("MJPEG stream error:", err.message);
+		res.end();
+	});
+
+	req.on("close", () => {
+		mjpegReq.destroy();
+	});
 });
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log('MJPEG proxy server is running');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`MJPEG proxy server is running on port ${PORT}`);
 });
