a2fd978536b4d16e335c420f3569990d6b087ebe
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-07-14 13:19:21 +0300

    create proxy server for camera stream

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import request from 'request';
+
+const app = express();
+app.use(cors());
+
+const MJPEG_SOURCE = 'http://212.112.136.4:83/mjpg/video.mjpg?camera=1';
+
+app.get('/cam', (req, res) => {
+	req.pipe(request(MJPEG_SOURCE)).pipe(res);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log('MJPEG proxy server is running');
+});
